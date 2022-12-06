ab7d630f35ffd26fdb286ba0f6d8fba033482ccf
kieran-woodrow <77970893+kieran-woodrow@users.noreply.github.com>
2022-12-07 00:00:39 +0200

    Completed Day 1

---
+++ 2022/1/colby.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+
+fs.readFile("input.txt", "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  var totalCaloriesOfEachElf = [];
+  var tally = 0;
+  var elfArray = data.split("\n");
+
+  for (var currentIndex = 0; currentIndex < elfArray.length; currentIndex++) {
+    if (elfArray[currentIndex] !== "") {
+      var intValue = parseInt(elfArray[currentIndex]);
+      tally += intValue;
+      if (currentIndex == elfArray.length - 1) {
+        totalCaloriesOfEachElf.push(tally);
+      }
+    } else {
+      totalCaloriesOfEachElf.push(tally);
+      tally = 0;
+    }
+  }
+
+  var sortedArrayInAscendingOrder = totalCaloriesOfEachElf.sort(
+    (a, b) => a - b
+  );
+  var highestTally = sortedArrayInAscendingOrder.pop();
+  console.log("\n");
+  console.log(
+    "The elf with the most calories had " + highestTally + " calories"
+  );
+  console.log("\n");
+});
