9e99e34221a0893c1784ed0e0360afff4cc1a661
kieran-woodrow <104440664+ColbyNel@users.noreply.github.com>
2022-12-13 23:59:55 +0200

    Stuff

--- 2022/Day3/Part1/Part1.js
+++ 2022/Day3/Part1/Part1.js
@@ -1,5 +1,63 @@
 const fs = require("fs");
 
+var commonLetter = [];
+var duplicateValue = 0;
+var alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "X",
+  "Y",
+  "Z",
+];
+
+//---------------------------
+//MAIN FUNCTIONS BELOW
+//---------------------------
 fs.readFile("input.txt", "utf8", (err, data) => {
   if (err) {
     console.error(err);
@@ -7,80 +65,46 @@ fs.readFile("input.txt", "utf8", (err, data) => {
   }
 
   var rucksackArray = data.split("\n");
-  var commonLetter = [];
-  var alphabet = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "X",
-    "Y",
-    "Z",
-  ];
 
   for (var rucksack = 0; rucksack < rucksackArray.length; rucksack++) {
     problemSolver(rucksackArray[rucksack]);
   }
 
-  function problemSolver(inputArray) {
-    var splitArray1 = inputArray.slice(0, Math.ceil(inputArray.length / 2));
-    var splitArray2 = inputArray.slice(
-      Math.ceil(inputArray.length / 2),
-      inputArray.length
-    );
-
-    for (var i = 0; i < splitArray1.length; i++) {
-      for (var j = 0; j < splitArray2.length; j++) {
-        if (splitArray1[i] === splitArray2[j]) {
-          commonLetter.push(splitArray1[i]);
-        }
-      }
-    }
-    return commonLetter;
-  }
-
   console.log(commonLetter);
+  convertArray(commonLetter);
+  console.log(rucksackArray.pop());
 });
+
+//---------------------------
+//HELPER FUNCTIONS BELOW
+//---------------------------
+
+function convertArray(commonLetter) {
+  for (var letter = 0; letter < commonLetter.length; letter++) {
+    // console.log(letter);
+    // console.log(commonLetter[letter]);
+
+    duplicateValue += alphabet.indexOf(commonLetter[letter]) + 1;
+  }
+  console.log(duplicateValue);
+}
+
+function problemSolver(inputArray) {
+  var splitArray1 = inputArray.slice(0, inputArray.length / 2);
+  var splitArray2 = inputArray.slice(inputArray.length / 2, inputArray.length);
+
+  for (var i = 0; i < splitArray1.length; i++) {
+    for (var j = 0; j < splitArray2.length; j++) {
+      if (
+        splitArray1[i] === splitArray2[j] &&
+        !commonLetter.includes(splitArray1[i])
+      ) {
+        commonLetter.push(splitArray1[i]);
+        // break;
+      }
+      //break;
+    }
+  }
+  //return commonLetter;
+  // convertArray(commonLetter);
+}
