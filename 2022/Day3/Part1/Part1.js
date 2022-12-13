fe3a9f131ba4969344693fded688ead9aea15670
kieran-woodrow <104440664+ColbyNel@users.noreply.github.com>
2022-12-13 21:12:40 +0200

    Update Part1.js

--- 2022/Day3/Part1/Part1.js
+++ 2022/Day3/Part1/Part1.js
@@ -1,29 +1,86 @@
-var exampleArray = ["WrThewTs","sThVzT","qwEpLFdqol","AskA","qwASWQrasW"]
-var commonLetter = []
-var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
-"A","B","C","D","E","F","G","H","I","J","K","L","M","O","P","Q","R","S","T","U","V","X","Y","Z"];
+const fs = require("fs");
 
+fs.readFile("input.txt", "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
 
-for (var rucksack = 0; rucksack < exampleArray.length; rucksack++){
-  problemSolver(exampleArray[rucksack])
-}
+  var rucksackArray = data.split("\n");
+  var commonLetter = [];
+  var alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "X",
+    "Y",
+    "Z",
+  ];
 
+  for (var rucksack = 0; rucksack < rucksackArray.length; rucksack++) {
+    problemSolver(rucksackArray[rucksack]);
+  }
 
-function problemSolver(inputArray){
-  var splitArray1 = inputArray.slice(0, Math.ceil(inputArray.length / 2))
-  var splitArray2 = inputArray.slice(Math.ceil(inputArray.length / 2),inputArray.length)
+  function problemSolver(inputArray) {
+    var splitArray1 = inputArray.slice(0, Math.ceil(inputArray.length / 2));
+    var splitArray2 = inputArray.slice(
+      Math.ceil(inputArray.length / 2),
+      inputArray.length
+    );
 
-  for (var i=0; i < splitArray1.length; i++){
-    for (var j=0; j<splitArray2.length; j++){
-      if (splitArray1[i] === splitArray2[j]){
-         commonLetter.push(splitArray1[i])
-      }
-      else {
-        console.log("Incorrect")
+    for (var i = 0; i < splitArray1.length; i++) {
+      for (var j = 0; j < splitArray2.length; j++) {
+        if (splitArray1[i] === splitArray2[j]) {
+          commonLetter.push(splitArray1[i]);
+        }
       }
     }
+    return commonLetter;
   }
-  return commonLetter
-}
 
-console.log(commonLetter)
+  console.log(commonLetter);
+});
